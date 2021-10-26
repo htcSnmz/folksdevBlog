5f3e3d06ed8e44c82267a6f95d70626a889dcf85
Hatice Sönmez <htc.sonmez@hotmail.com>
2021-10-26 21:37:35 +0300

    entities added

---
+++ src/main/java/com/folksdev/blog/model/Blog.kt
@@ -0,0 +1,23 @@
+package com.folksdev.blog.model
+
+import org.hibernate.annotations.GenericGenerator
+import javax.persistence.*
+
+@Entity
+data class Blog @JvmOverloads constructor(
+        @Id
+        @Column(name = "blog_id")
+        @GeneratedValue(generator = "UUID")
+        @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+        val id: String? = "",
+        val title: String,
+        val contents: String,
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "writer_id")
+        val writer: Writer,
+
+
+
+) {
+}
---
+++ src/main/java/com/folksdev/blog/model/Writer.kt
@@ -0,0 +1,22 @@
+package com.folksdev.blog.model
+
+import org.hibernate.annotations.GenericGenerator
+import javax.persistence.*
+
+@Entity
+data class Writer @JvmOverloads constructor(
+        @Id
+        @Column(name = "writer_id")
+        @GeneratedValue(generator = "UUID")
+        @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+        val id: String? = "",
+        val name: String,
+        val surname: String,
+
+        @OneToMany(mappedBy = "writer", fetch = FetchType.LAZY)
+        val blogs: Set<Blog>? = HashSet()
+
+
+){
+
+}
